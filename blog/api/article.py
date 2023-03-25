7e22ba8e6d309e988d2641ce6411694adab164d5
AnnaKalashnikova <kushina1809@gmail.com>
2023-03-25 21:24:46 +0300

    Lesson 10 flask-combo-jsonapi

--- blog/api/article.py
+++ blog/api/article.py
@@ -2,10 +2,18 @@ from flask_combo_jsonapi import ResourceDetail, ResourceList
 from blog.schemas import ArticleSchema
 from blog.models.database import db
 from blog.models import Article
+from combojsonapi.event.resource import EventsResource
+from blog.permissions.article import ArticlePermission
+
+
+class ArticleListEvents(EventsResource):
+    def event_get_count(self):
+        return {"count": Article.query.count()}
 
 
 class ArticleList(ResourceList):
     schema = ArticleSchema
+    events = ArticleListEvents
     data_layer = {
         "session": db.session,
         "model": Article,
@@ -17,4 +25,5 @@ class ArticleDetail(ResourceDetail):
     data_layer = {
         "session": db.session,
         "model": Article,
+        "permission_get": [ArticlePermission],
     }
